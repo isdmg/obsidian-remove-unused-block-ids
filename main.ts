6b6468e40280afb97cdda02fbb5446b3961e255f
daniel-geneta <daniel.geneta.m@gmail.com>
2024-09-23 11:55:48 +0800

    Add local changes

--- main.ts
+++ main.ts
@@ -1,134 +1,281 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, TFile, Notice, Modal, Setting, PluginSettingTab } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface UnusedBlockId {
+    id: string;
+    file: string;
+    line: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface UnusedBlockIdRemoverSettings {
+    excludedExtensions: string[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+const DEFAULT_SETTINGS: Partial<UnusedBlockIdRemoverSettings> = {
+    excludedExtensions: ['.excalidraw.md']
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class UnusedBlockIdRemoverSettingTab extends PluginSettingTab {
+    plugin: UnusedBlockIdRemover;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: UnusedBlockIdRemover) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+    display(): void {
+        const { containerEl } = this;
+
+        containerEl.empty();
+
+        containerEl.createEl('h2', { text: 'Unused Block ID Remover Settings' });
+
+        new Setting(containerEl)
+            .setName('Excluded File Extensions')
+            .setDesc('Add file extensions (e.g., .excalidraw.md) separated by commas to exclude from scanning.')
+            .addTextArea((text) => {
+                text
+                    .setPlaceholder('Enter extensions separated by commas')
+                    .setValue(this.plugin.settings.excludedExtensions.join(', '))
+                    .onChange(async (value) => {
+                        this.plugin.settings.excludedExtensions = value.split(',').map(ext => ext.trim());
+                        await this.plugin.saveSettings();
+                    });
+            });
+    }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ConfirmationModal extends Modal {
+    plugin: UnusedBlockIdRemover;
+    unusedBlockIds: UnusedBlockId[];
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+    constructor(plugin: UnusedBlockIdRemover, unusedBlockIds: UnusedBlockId[]) {
+        super(plugin.app);
+        this.plugin = plugin;
+        this.unusedBlockIds = unusedBlockIds;
+    }
 
-	display(): void {
-		const {containerEl} = this;
+    onOpen() {
+        const { contentEl, modalEl } = this;
+        contentEl.empty();
+        contentEl.createEl('h2', { text: 'Unused Block IDs' });
 
-		containerEl.empty();
+        // Make the modal draggable
+        this.makeDraggable(modalEl);
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        const list = contentEl.createEl('ul');
+        this.unusedBlockIds.forEach(item => {
+            const li = list.createEl('li');
+            const link = li.createEl('a', {
+                text: item.id,
+                href: '#'
+            });
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                this.plugin.openFileAtBlockId(item.file, item.id);
+            });
+            li.createEl('span', { text: ` in file: ${item.file}` });
+        });
+
+        new Setting(contentEl)
+            .addButton(btn => btn
+                .setButtonText('Delete All')
+                .onClick(() => {
+                    this.plugin.deleteUnusedBlockIds(this.unusedBlockIds);
+                    this.close();
+                }))
+            .addButton(btn => btn
+                .setButtonText('Cancel')
+                .onClick(() => this.close()));
+    }
+
+    onClose() {
+        const { contentEl } = this;
+        contentEl.empty();
+    }
+
+    makeDraggable(modalEl: HTMLElement) {
+        let isDragging = false;
+        let offsetX = 0;
+        let offsetY = 0;
+
+        modalEl.style.position = 'absolute';
+
+        modalEl.addEventListener('mousedown', (e) => {
+            isDragging = true;
+            offsetX = e.clientX - modalEl.getBoundingClientRect().left;
+            offsetY = e.clientY - modalEl.getBoundingClientRect().top;
+            modalEl.style.cursor = 'move';
+        });
+
+        document.addEventListener('mousemove', (e) => {
+            if (isDragging) {
+                modalEl.style.left = `${e.clientX - offsetX}px`;
+                modalEl.style.top = `${e.clientY - offsetY}px`;
+            }
+        });
+
+        document.addEventListener('mouseup', () => {
+            isDragging = false;
+            modalEl.style.cursor = 'default';
+        });
+    }
+}
+
+export default class UnusedBlockIdRemover extends Plugin {
+    settings: UnusedBlockIdRemoverSettings;
+
+    async onload() {
+        await this.loadSettings();
+
+        this.addSettingTab(new UnusedBlockIdRemoverSettingTab(this.app, this));
+
+        this.addCommand({
+            id: 'scan-vault',
+            name: 'Scan Vault',
+            callback: () => this.findUnusedBlockIds(),
+        });
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign(
+            {},
+            DEFAULT_SETTINGS,
+            await this.loadData()
+        );
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+
+    async findUnusedBlockIds() {
+        const loadingNotice = new Notice('Searching for unused block IDs...', 0);
+
+        try {
+            // If the excludedExtensions setting is empty, include all files
+            const excludedExtensions = this.settings.excludedExtensions.filter(ext => ext); // Filter out empty strings
+            const files = this.app.vault.getMarkdownFiles().filter(file => {
+                // If there are no excluded extensions, return true (include all files)
+                if (excludedExtensions.length === 0) {
+                    return true;
+                }
+                // Otherwise, exclude files that end with any of the extensions
+                return !excludedExtensions.some(ext => file.path.endsWith(ext));
+            });
+
+            const blockIds = new Map<string, UnusedBlockId>();
+            const blockIdReferences = new Set<string>();
+
+            for (const file of files) {
+                const content = await this.app.vault.cachedRead(file);
+                this.collectBlockIdsAndReferences(content, file.path, blockIds, blockIdReferences);
+            }
+
+            const unusedBlockIds = Array.from(blockIds.values())
+                .filter(item => !blockIdReferences.has(item.id));
+
+            loadingNotice.hide();
+
+            // If no unused block IDs are found, show a notice
+            if (unusedBlockIds.length === 0) {
+                new Notice('No unused block IDs found.');
+            } else {
+                // If unused block IDs are found, show the confirmation modal
+                new ConfirmationModal(this, unusedBlockIds).open();
+            }
+        } catch (error) {
+            loadingNotice.hide();
+            new Notice(`Error: ${error.message}`);
+        }
+    }
+
+    collectBlockIdsAndReferences(
+        content: string,
+        filePath: string,
+        blockIds: Map<string, UnusedBlockId>,
+        blockIdReferences: Set<string>
+    ) {
+        const lines = content.split('\n');
+        const blockIdRegex = /\^([\w-]+)$/;  // Matches block IDs like ^blockID
+        const blockIdRefRegex = /\[\[(.*?)#\^([\w-]+)\s*(\|.*?)?\]\]/g;  // Updated to handle spaces around |
+
+        lines.forEach((line, index) => {
+            // Match block IDs at the end of the line, e.g., ^blockID
+            const match = line.match(blockIdRegex);
+            if (match && this.isValidBlockId(match[1])) {
+                blockIds.set(match[1], {
+                    id: match[1],
+                    file: filePath,
+                    line: line.trim()
+                });
+            }
+
+            // Match block references, e.g., [[filename#^blockID | optional text]]
+            let refMatch;
+            while ((refMatch = blockIdRefRegex.exec(line)) !== null) {
+                blockIdReferences.add(refMatch[2]);  // refMatch[2] captures the block ID
+            }
+        });
+    }
+
+    isValidBlockId(id: string): boolean {
+        return /^[\w-]+$/.test(id);
+    }
+
+    async deleteUnusedBlockIds(unusedBlockIds: UnusedBlockId[]) {
+        const loadingNotice = new Notice('Deleting unused block IDs...', 0);
+        let totalRemoved = 0;
+        const fileChanges = new Map<string, string[]>();
+
+        try {
+            for (const item of unusedBlockIds) {
+                if (!fileChanges.has(item.file)) {
+                    const file = this.app.vault.getAbstractFileByPath(item.file);
+                    if (file instanceof TFile) {
+                        const content = await this.app.vault.read(file);
+                        fileChanges.set(item.file, content.split('\n'));
+                    }
+                }
+
+                const lines = fileChanges.get(item.file);
+                if (lines) {
+                    const index = lines.findIndex(line => line.includes(`^${item.id}`));
+                    if (index !== -1) {
+                        lines[index] = lines[index].replace(/\s*\^[\w-]+$/, '');
+                        totalRemoved++;
+                    }
+                }
+            }
+
+            for (const [filePath, lines] of fileChanges) {
+                const file = this.app.vault.getAbstractFileByPath(filePath);
+                if (file instanceof TFile) {
+                    await this.app.vault.modify(file, lines.join('\n'));
+                }
+            }
+
+            loadingNotice.hide();
+            new Notice(`Removed ${totalRemoved} unused block IDs.`);
+        } catch (error) {
+            loadingNotice.hide();
+            new Notice(`Error: ${error.message}`);
+        }
+    }
+
+    async openFileAtBlockId(filePath: string, blockId: string) {
+        const file = this.app.vault.getAbstractFileByPath(filePath);
+        if (file instanceof TFile) {
+            const leaf = this.app.workspace.getLeaf();
+            await leaf.openFile(file, {
+                eState: { line: await this.getLineNumberForBlockId(file, blockId) }
+            });
+        }
+    }
+
+    async getLineNumberForBlockId(file: TFile, blockId: string): Promise<number> {
+        const content = await this.app.vault.cachedRead(file);
+        const lines = content.split('\n');
+        const index = lines.findIndex(line => line.includes(`^${blockId}`));
+        return index !== -1 ? index : 0;
+    }
 }
